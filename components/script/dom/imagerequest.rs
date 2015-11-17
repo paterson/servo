2efb89e6583559c25dcdca7b3adad38419a4dc4a
Tomas Barry <tomas.barry@hotmail.com>
2015-11-17 03:13:38 -0800

    Removed unused uses from imagerequest.rs

--- components/script/dom/imagerequest.rs
+++ components/script/dom/imagerequest.rs
@@ -2,37 +2,9 @@
 * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use dom::attr::Attr;
-use dom::attr::AttrValue;
-use dom::bindings::cell::DOMRefCell;
-use dom::bindings::codegen::Bindings::HTMLImageElementBinding;
-use dom::bindings::codegen::Bindings::HTMLImageElementBinding::HTMLImageElementMethods;
-use dom::bindings::codegen::Bindings::WindowBinding::WindowMethods;
-use dom::bindings::codegen::InheritTypes::{ElementCast, EventTargetCast, NodeCast};
-use dom::bindings::codegen::InheritTypes::{HTMLElementCast, HTMLImageElementDerived};
-use dom::bindings::error::Fallible;
-use dom::bindings::js::{LayoutJS, Root};
-use dom::bindings::refcounted::Trusted;
-use dom::document::Document;
-use dom::element::{AttributeMutation, ElementTypeId};
-use dom::event::{Event, EventBubbles, EventCancelable};
-use dom::eventtarget::{EventTarget, EventTargetTypeId};
-use dom::htmlelement::{HTMLElement, HTMLElementTypeId};
-use dom::node::{Node, NodeDamage, NodeTypeId, document_from_node, window_from_node};
-use dom::virtualmethods::VirtualMethods;
-use ipc_channel::ipc;
-use ipc_channel::router::ROUTER;
-use net_traits::image::base::Image;
-use net_traits::image_cache_task::{ImageResponder, ImageResponse};
-use script_task::ScriptTaskEventCategory::UpdateReplacedElement;
-use script_task::{CommonScriptMsg, Runnable, ScriptChan};
-use std::borrow::ToOwned;
-use std::sync::Arc;
-use string_cache::Atom;
-use url::{Url, UrlParser};
-use util::str::DOMString;
+
+use url::Url;
 use dom::imagedata::ImageData;
-use dom::bindings::global::GlobalRef;
 
 
 // Unavailable
