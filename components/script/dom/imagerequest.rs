ad7e5581f609e5df1d99559e69abb508333cd6f0
Niall Paterson <npaters@tcd.ie>
2015-11-26 12:00:54 +0000

    Updated imagerequest.rs

--- components/script/dom/imagerequest.rs
+++ components/script/dom/imagerequest.rs
@@ -3,9 +3,13 @@
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 
-use url::Url;
+use dom::url::URL;
+use dom::bindings::global::GlobalRef;
 use dom::imagedata::ImageData;
-
+use dom::bindings::codegen::Bindings::ImageDataBinding::ImageDataMethods;
+use dom::bindings::reflector::{Reflector, reflect_dom_object};
+use dom::bindings::codegen::Bindings::ImageRequestBinding;
+use dom::bindings::codegen::Bindings::ImageRequestBinding::ImageRequestMethods;
 
 // Unavailable
 //     The user agent hasn't obtained any image data, or has obtained some or all of the 
@@ -19,8 +23,15 @@ use dom::imagedata::ImageData;
 //     decode the image enough to get the image dimensions (e.g. the image is corrupted, or the format 
 //         is not supported, or no data could be obtained).
 
-#[must_root]
-#[derive(JSTraceable, Clone, HeapSizeOf)]
+#[dom_struct]
+pub struct ImageRequest {
+  reflector_: Reflector,
+  state : ImageState,
+  currentUrl : URL,
+  imageData : ImageData,
+}
+
+//#[derive(JSTraceable, Clone, HeapSizeOf)]
 pub enum ImageState {
    Unavailable,
    PartiallyAvailable,
@@ -28,20 +39,13 @@ pub enum ImageState {
    Broken,
 }
 
-#[dom_struct]
-pub struct ImageRequest {
-   state : ImageState,
-   currentUrl : Url,
-   imageData : ImageData,
-}
-
-
 impl ImageRequest {
-   fn new(imageData : ImageData, currentUrl : Url) -> ImageRequest {
+   fn new(global: GlobalRef, imageData : ImageData, currentUrl : DOMString) -> ImageRequest {
        ImageRequest {
-           state : ImageState { Unavailable },
-           currentUrl : currentUrl,
-           imageData : imageData,
+          reflector_: Reflector::new(),
+          state : ImageState::Unavailable,
+          currentUrl : currentUrl,
+          imageData : imageData,
        }
    }
 }
@@ -53,31 +57,11 @@ impl ImageRequestMethods for ImageRequest {
        self.state
    }
 
-   fn CurrentUrl(&self) -> String {
+   fn CurrentUrl(&self) -> URL {
        self.currentUrl
    }
 
    fn ImageData(&self) -> ImageData {
        self.imageData
-   }
-
-   fn SetStateToUnavailable() {
-       self.state = ImageState { Unavailable }
-   }
-
-   fn SetStateToPartiallyAvailable() {
-       self.state = ImageState { PartiallyAvailable }
-   }
-
-   fn SetStateToCompletelyAvailable() {
-       self.state = ImageState { CompletelyAvailable }
-   }
-
-   fn SetStateToBroken() {
-       self.state = ImageState { Broken }
-   }
-
-   fn SetCurrentUrl(newUrl : String) {
-       self.currentUrl = newUrl
    }
 }
